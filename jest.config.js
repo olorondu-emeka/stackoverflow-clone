992ff21e852285a06de14c3ba069f42c38c68129
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2021-04-05 03:54:38 +0100

    updated dependencies and eslint config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+	coverageDirectory: 'coverage',
+	moduleDirectories: ['node_modules', 'src'],
+	roots: ['<rootDir>/src'],
+	testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+	transform: {
+		'^.+\\.(ts|tsx)$': 'ts-jest'
+	},
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
+};
