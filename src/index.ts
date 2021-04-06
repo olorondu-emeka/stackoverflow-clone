2c3f5bd53d80f9a5802ebe885f82a18ad8ec0066
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2021-04-06 03:53:08 +0100

    added the index.ts file

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express, {
+  json,
+  urlencoded,
+  Request,
+  Response,
+  NextFunction
+} from 'express';
+import cors from 'cors';
+
+import routes from 'entrypoint/web/routes';
+
+const app = express();
+const PORT = Number(process.env.PORT) || 5000;
+// const developmentServer = `http://localhost:${PORT}`;
+
+app.use(cors());
+app.use(cors());
+app.use(json({ limit: '20mb' }));
+app.use(urlencoded({ extended: true }));
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send('server error, this will be resolved shortly!');
+
+  next();
+});
+
+app.use('/api/v1', routes);
+
+app.use('*', (request, response) => {
+  response.status(404).send('Not Found');
+});
+
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.info(`Server started on port ${PORT}`));
+
+export default app;
